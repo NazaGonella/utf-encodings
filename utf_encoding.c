d13bd4352a46239f79779b38a453703fc238a043
NazaGonella <nazagonella2@gmail.com>
2025-12-05 21:45:29 -0300

    modified names

--- utf_encoding.c
+++ utf_encoding.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 
-int CodepointToUTF32LE(unsigned int codepoint, unsigned char *output) {
+int CodepointToUTF32LE(unsigned int codepoint, unsigned char *dst) {
 
     if (codepoint >= 0x0 && codepoint <= 0x10FFFF) {
-        output[0] = codepoint & 0xFF;
-        output[1] = (codepoint >> 8) & 0xFF;
-        output[2] = (codepoint >> 16) & 0xFF;
-        output[3] = (codepoint >> 24) & 0xFF;
+        dst[0] = codepoint & 0xFF;
+        dst[1] = (codepoint >> 8) & 0xFF;
+        dst[2] = (codepoint >> 16) & 0xFF;
+        dst[3] = (codepoint >> 24) & 0xFF;
         return 4;
     }
 
@@ -16,13 +16,13 @@ int CodepointToUTF32LE(unsigned int codepoint, unsigned char *output) {
 }
 
 
-int CodepointToUTF32BE(unsigned int codepoint, unsigned char *output) {
+int CodepointToUTF32BE(unsigned int codepoint, unsigned char *dst) {
 
     if (codepoint >= 0x0 && codepoint <= 0x10FFFF) {
-        output[0] = (codepoint >> 24) & 0xFF;
-        output[1] = (codepoint >> 16) & 0xFF;
-        output[2] = (codepoint >> 8) & 0xFF;
-        output[3] = codepoint & 0xFF;
+        dst[0] = (codepoint >> 24) & 0xFF;
+        dst[1] = (codepoint >> 16) & 0xFF;
+        dst[2] = (codepoint >> 8) & 0xFF;
+        dst[3] = codepoint & 0xFF;
         return 4;
     }
 
@@ -31,12 +31,12 @@ int CodepointToUTF32BE(unsigned int codepoint, unsigned char *output) {
 }
 
 
-int CodepointToUTF16LE(unsigned int codepoint, unsigned char *output) {
+int CodepointToUTF16LE(unsigned int codepoint, unsigned char *dst) {
 
     if (codepoint <= 0xFFFF) {
         if (codepoint >= 0xD800 && codepoint <= 0xDFFF) return 0;
-        output[0] = (unsigned char)(codepoint & 0xFF);
-        output[1] = (unsigned char)((codepoint >> 8) & 0xFF);
+        dst[0] = (unsigned char)(codepoint & 0xFF);
+        dst[1] = (unsigned char)((codepoint >> 8) & 0xFF);
         return 2;
     }
     else if (codepoint <= 0x10FFFF) {
@@ -44,10 +44,10 @@ int CodepointToUTF16LE(unsigned int codepoint, unsigned char *output) {
         unsigned int high = (0b110110 << 10) | ((codepoint_u >> 10) & 0x3FF);
         unsigned int low  = (0b110111 << 10) | (codepoint_u & 0x3FF);
 
-        output[0] = high & 0xFF;
-        output[1] = (high >> 8) & 0xFF;
-        output[2] = low & 0xFF;
-        output[3] = (low >> 8) & 0xFF;
+        dst[0] = high & 0xFF;
+        dst[1] = (high >> 8) & 0xFF;
+        dst[2] = low & 0xFF;
+        dst[3] = (low >> 8) & 0xFF;
         return 4;
     }
 
@@ -56,12 +56,12 @@ int CodepointToUTF16LE(unsigned int codepoint, unsigned char *output) {
 }
 
 
-int CodepointToUTF16BE(unsigned int codepoint, unsigned char *output) {
+int CodepointToUTF16BE(unsigned int codepoint, unsigned char *dst) {
 
     if (codepoint <= 0xFFFF) {
         if (codepoint >= 0xD800 && codepoint <= 0xDFFF) return 0;
-        output[0] = (unsigned char)((codepoint >> 8) & 0xFF);
-        output[1] = (unsigned char)(codepoint & 0xFF);
+        dst[0] = (unsigned char)((codepoint >> 8) & 0xFF);
+        dst[1] = (unsigned char)(codepoint & 0xFF);
         return 2;
     }
     else if (codepoint <= 0x10FFFF) {
@@ -69,10 +69,10 @@ int CodepointToUTF16BE(unsigned int codepoint, unsigned char *output) {
         unsigned int high = (0b110110 << 10) | ((codepoint_u >> 10) & 0x3FF);
         unsigned int low  = (0b110111 << 10) | (codepoint_u & 0x3FF);
 
-        output[0] = (high >> 8) & 0xFF;
-        output[1] = high & 0xFF;
-        output[2] = (low >> 8) & 0xFF;
-        output[3] = low & 0xFF;
+        dst[0] = (high >> 8) & 0xFF;
+        dst[1] = high & 0xFF;
+        dst[2] = (low >> 8) & 0xFF;
+        dst[3] = low & 0xFF;
         return 4;
     }
 
@@ -81,25 +81,25 @@ int CodepointToUTF16BE(unsigned int codepoint, unsigned char *output) {
 }
 
 
-int CodepointToUTF8(unsigned int codepoint, unsigned char *output) {
+int CodepointToUTF8(unsigned int codepoint, unsigned char *dst) {
 
     if (codepoint <= 0x7F) {
-        output[0] = (unsigned char)codepoint;
+        dst[0] = (unsigned char)codepoint;
         return 1;
     } else if (codepoint <= 0x7FF) {
-        output[0] = (unsigned char)(0b11000000 | ((codepoint >> 6) & 0x1F));    // (110)0 0000 | 000x xxxx
-        output[1] = (unsigned char)(0b10000000 | (codepoint & 0x3F));           // (10)00 0000 | 00xx xxxx
+        dst[0] = (unsigned char)(0b11000000 | ((codepoint >> 6) & 0x1F));    // (110)0 0000 | 000x xxxx
+        dst[1] = (unsigned char)(0b10000000 | (codepoint & 0x3F));           // (10)00 0000 | 00xx xxxx
         return 2;
     } else if (codepoint <= 0xFFFF) {
-        output[0] = (unsigned char)(0b11100000 | ((codepoint >> 12) & 0x0F));   // (1110) 0000 | 0000 xxxx
-        output[1] = (unsigned char)(0b10000000 | ((codepoint >> 6) & 0x3F));    // (10)00 0000 | 00xx xxxx
-        output[2] = (unsigned char)(0b10000000 | (codepoint & 0x3F));           // (10)00 0000 | 00xx xxxx
+        dst[0] = (unsigned char)(0b11100000 | ((codepoint >> 12) & 0x0F));   // (1110) 0000 | 0000 xxxx
+        dst[1] = (unsigned char)(0b10000000 | ((codepoint >> 6) & 0x3F));    // (10)00 0000 | 00xx xxxx
+        dst[2] = (unsigned char)(0b10000000 | (codepoint & 0x3F));           // (10)00 0000 | 00xx xxxx
         return 3;
     } else if (codepoint <= 0x10FFFF) {
-        output[0] = (unsigned char)(0b11110000 | ((codepoint >> 18) & 0x07));   // (1111 0)000 | 0000 0xxx
-        output[1] = (unsigned char)(0b10000000 | ((codepoint >> 12) & 0x3F));   // (10)00 0000 | 00xx xxxx
-        output[2] = (unsigned char)(0b10000000 | ((codepoint >> 6) & 0x3F));    // (10)00 0000 | 00xx xxxx
-        output[3] = (unsigned char)(0b10000000 | (codepoint & 0x3F));           // (10)00 0000 | 00xx xxxx
+        dst[0] = (unsigned char)(0b11110000 | ((codepoint >> 18) & 0x07));   // (1111 0)000 | 0000 0xxx
+        dst[1] = (unsigned char)(0b10000000 | ((codepoint >> 12) & 0x3F));   // (10)00 0000 | 00xx xxxx
+        dst[2] = (unsigned char)(0b10000000 | ((codepoint >> 6) & 0x3F));    // (10)00 0000 | 00xx xxxx
+        dst[3] = (unsigned char)(0b10000000 | (codepoint & 0x3F));           // (10)00 0000 | 00xx xxxx
         return 4;
     }
 
